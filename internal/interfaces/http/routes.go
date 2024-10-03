860f1dc9bfc4ee8f7ca5c979a493c24c61d67fad
fernando <fernandolopezchs@hotmail.com>
2024-10-03 12:07:44 -0700

    - adding cors

--- internal/interfaces/http/routes.go
+++ internal/interfaces/http/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris/v12"
 	"league-management/internal/application/service"
 	"league-management/internal/interfaces/http/controllers"
@@ -30,7 +31,16 @@ func loginEndpoint(ctx iris.Context) {
 
 func Initialize() *iris.Application {
 	app := iris.New()
+
 	app.Use(iris.Compression)
+
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8081"},                   // Specify the origin explicitly
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Specify allowed methods
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},           // Specify allowed headers
+		AllowCredentials: true,                                                // Allow credentials
+	})
+	app.UseRouter(crs)
 
 	registerV1API(app)
 
