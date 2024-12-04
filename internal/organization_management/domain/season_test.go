6751a8c39505309fdc9557b2a67cab07f11f4637
fernando <fernandolopezchs@hotmail.com>
2024-12-04 12:19:51 -0800

    TDD.

--- internal/organization_management/domain/season_test.go
+++ internal/organization_management/domain/season_test.go
@@ -7,6 +7,66 @@ import (
 
 func TestSeason_ScheduleRounds(t *testing.T) {
 	t.Run("Zero teams should error out", func(t *testing.T) {
-		assert.True(t, true)
+		season := Season{
+			ID:       "id-1",
+			LeagueId: "league-id-1",
+			Name:     "Test League",
+			Rounds:   nil,
+		}
+
+		leagueId := "league-id"
+		league := League{
+			Id:             &leagueId,
+			Name:           "test-league",
+			OwnerId:        "test-owner-id",
+			OrganizationId: "test-org-id",
+			Memberships:    []LeagueMembership{},
+		}
+
+		err := season.ScheduleRounds(league)
+		assert.Error(t, err)
+	})
+
+	t.Run("Season with even teams without rules", func(t *testing.T) {
+		season := Season{
+			ID:       "id-1",
+			LeagueId: "league-id-1",
+			Name:     "Test League",
+		}
+
+		member1 := LeagueMembership{
+			ID:     "test-id",
+			TeamID: "team A",
+		}
+		member2 := LeagueMembership{
+			ID:     "test-id",
+			TeamID: "team B",
+		}
+		member3 := LeagueMembership{
+			ID:     "test-id",
+			TeamID: "team C",
+		}
+		member4 := LeagueMembership{
+			ID:     "test-id",
+			TeamID: "team D",
+		}
+
+		leagueId := "league-id"
+		league := League{
+			Id:             &leagueId,
+			Name:           "test-league",
+			OwnerId:        "test-owner-id",
+			OrganizationId: "test-org-id",
+			Memberships:    []LeagueMembership{member1, member2, member3, member4},
+		}
+
+		err := season.ScheduleRounds(league)
+
+		firstRound := season.Rounds[0]
+
+		assert.NoError(t, err)
+		assert.Len(t, season.Rounds, 3)
+		assert.Equal(t, 1, firstRound.RoundNumber)
+		assert.Len(t, firstRound.Matches, 2)
 	})
 }
